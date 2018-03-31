21386b5a01cb483abefabff4315b27d46532c07f
K'winLook <chakkaphat060@gmail.com>
2018-03-31 12:02:54 +0700

    ก่อนเที่ยง

--- src/list.js
+++ src/list.js
@@ -1,28 +1,31 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet
-} from 'react-native';
+import {View,Text,StyleSheet,TouchableOpacity} from 'react-native';
 export default class List extends React.Component {
-  constructor(){
-    super();
-    this.state = {list:['Click to Remove' , 'แปรงฟัน' , 'อาบน้ำ'] }
-
-  }
-  render() {
-    return (
-      <View>
-            {this.state.list.map((text,i)=>
-              (
-                <View key={i} style={{backgroundColor:'whitesmoke',padding:15,marginBottom: 5}}>
-                    <Text>{text}</Text>
-                </View>
-              )
-            )}
-      </View>
-    )
-  }
+    constructor(){
+        super();
+        this.state = {list:['Click to Remove','แปรงฟัน','อาบน้ำ'],}
+        this.removeTodo = this.removeTodo.bind(this);
+        this
+    }
+    removeTodo(index){
+        let tmpList = [...this.state.list];
+        tmpList = tmpList.filter((tmp,i)=>i!=index)
+        this.setState({list:tmpList})
+    }
+    render(){
+        return(
+            <View>
+                {this.state.list.map((text,i)=>
+                    (
+                    <TouchableOpacity key={i} onPress={()=>this.removeTodo(i)}
+                    style={{backgroundColor:'whitesmoke',padding:15,marginBottom:5}} >
+                        <Text>{text}</Text>
+                    </TouchableOpacity>
+                    )
+                )}
+            </View>
+        )
+    }
 }
 const styles = StyleSheet.create({
 
